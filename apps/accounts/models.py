7bfc02abe0fa840b664b1dea2ae5c1d68a391140
Arash F <arash@hami.blue>
2021-01-03 13:18:29 +0330

    Added Order Model

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -12,3 +12,4 @@ class User(AbstractUser):
     )
 
     role = models.PositiveSmallIntegerField(_('role'), choices=ROLE_CHOICES, default=ROLE_GUEST)
+    address = models.TextField(_('address'), blank=True)
--- apps/orders/models.py
+++ apps/orders/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.postgres.fields import DateTimeRangeField
 
-# Create your models here.
+
+class Order(models.Model):
+    created_time = models.DateTimeField(_('created time'), auto_now_add=True)
+    updated_time = models.DateTimeField(_('updated time'), auto_now=True)
+    guest = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    restaurant = models.ForeignKey('restaurants.Restaurant', on_delete=models.PROTECT)
+    reservation_time = DateTimeRangeField(_('reservation time'))
+    is_takeout = models.BooleanField(_('is takeout'), default=False)
+
+    foods = models.ManyToManyField('restaurants.Food', blank=False)
